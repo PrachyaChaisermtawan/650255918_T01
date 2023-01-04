087d2fcc30ea1f5cb7c2aaffcfdf69835bc24ded
PrachyaChaisermtawan <65025918@up.ac.th>
2023-01-04 11:48:27 +0700

    เสร็จล้าา

--- Assignment01.js
+++ Assignment01.js
@@ -1,8 +1,8 @@
 let randNum = Math.floor(Math.random()*101);
-let x = 0;
+let x = 1;
 function GN(){
     const Guess_Number = document.getElementById("G_N").value;
-    if (Guess_Number == randNum){alert("ถูกต้อง")}
-    else if(Guess_Number>randNum){alert("มากกว่า")}
-    else if(Guess_Number<randNum){alert("น้อยกว่า")}
+    if (Guess_Number == randNum){alert("ถูกต้อง ค่าที่ถูกคือ : " + randNum +"คุณทายไปทั้งหมด : "+ x + "ครั้ง")}
+    else if(Guess_Number>randNum){x++;alert("มากกว่า")}
+    else if(Guess_Number<randNum){x++;alert("น้อยกว่า")}
 }
