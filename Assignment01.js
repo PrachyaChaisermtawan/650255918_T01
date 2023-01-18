3ef81814da836f7dacdc67900908caf31f0e0ef1
PrachyaChaisermtawan <65025918@up.ac.th>
2023-01-18 09:09:42 +0700

    Update Assignment01.js

--- Assignment01.js
+++ Assignment01.js
@@ -2,7 +2,7 @@ let randNum = Math.floor(Math.random()*101);
 let x = 1;
 function GN(){
     const Guess_Number = document.getElementById("G_N").value;
-    if (Guess_Number == randNum){alert("ถูกต้อง ค่าที่ถูกคือ : " + randNum +"คุณทายไปทั้งหมด : "+ x + "ครั้ง")}
+    if (Guess_Number == randNum){alert("ถูกต้อง ค่าที่ถูกคือ : " + randNum +"คุณทายไปทั้งหมด : "+ x + " ครั้ง")}
     else if(Guess_Number>randNum){x++;alert("มากกว่า")}
     else if(Guess_Number<randNum){x++;alert("น้อยกว่า")}
 }
